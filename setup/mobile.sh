391c5bc3daa6fb844d660945a65bad2787ab24df
Lily <egzozu.be.bas@gmail.com>
2025-02-13 01:36:50 +0300

    hxvlc 2.0.1 requirement

--- setup/mobile.sh
+++ setup/mobile.sh
@@ -15,7 +15,7 @@ haxelib install tjson 1.4.0 --quiet
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
 haxelib git linc_luajit https://github.com/MobilePorting/linc_luajit-0.7plus --quiet
 haxelib install hxdiscord_rpc 1.2.4 --quiet --skip-dependencies
-haxelib install hxvlc 1.9.2 --quiet --skip-dependencies
+haxelib install hxvlc 2.0.1 --quiet --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
 haxelib git extension-androidtools https://github.com/MAJigsaw77/extension-androidtools --quiet --skip-dependencies
